e0bac0e9d8867ebb8a3b0db9d3a871ad4566e763
fabiano <fabianofmarcos@gmail.com>
2020-03-27 22:36:42 -0300

    init project

---
+++ ProAgil.API/Controllers/WeatherForescastController.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using ProAgil.API.Model;
+
+namespace ProAgil.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Event>> Get()
+        {
+            return new Event[] {
+              new Event() {
+                EventId = 1,
+                Theme = "Curso dotnet",
+                Locale = "Lagoa Santa",
+                Lot = "1 Lote",
+                AmountPeoples = 250,
+                DateEvent = DateTime.Now.AddDays(2).ToString("dd/MM/yyyy")
+              },
+              new Event() {
+                EventId = 2,
+                Theme = "Curso Angular",
+                Locale = "Lagoa Santa",
+                Lot = "2 Lote",
+                AmountPeoples = 350,
+                DateEvent = DateTime.Now.AddDays(3).ToString("dd/MM/yyyy")
+              }
+            };
+        }
+
+        public ActionResult<Event> Get(int id) => new Event[] {
+              new Event() {
+                EventId = 1,
+                Theme = "Curso dotnet",
+                Locale = "Lagoa Santa",
+                Lot = "1 Lote",
+                AmountPeoples = 250,
+                DateEvent = DateTime.Now.AddDays(2).ToString("dd/MM/yyyy")
+              },
+              new Event() {
+                EventId = 2,
+                Theme = "Curso Angular",
+                Locale = "Lagoa Santa",
+                Lot = "2 Lote",
+                AmountPeoples = 350,
+                DateEvent = DateTime.Now.AddDays(3).ToString("dd/MM/yyyy")
+              }
+        }.FirstOrDefault(Event => Event.EventId == id);
+    }
+}
---
+++ ProAgil.API/Model/Event.cs
@@ -0,0 +1,12 @@
+namespace ProAgil.API.Model
+{
+    public class Event
+    {
+      public int EventId { get; set; }
+      public string Locale { get; set; }
+      public string DateEvent { get; set; }
+      public string Theme { get; set; }
+      public int AmountPeoples { get; set; }
+      public string Lot { get; set; }
+    }
+}
