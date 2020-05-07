3c3bb7f74eec8bf05c052c7f59f1a66f4d58c5cd
Fabiano Marcos <fabiano@vexter.com.br>
2020-05-06 22:22:24 -0300

    initial angular project

--- ProAgil.WebAPI/Startup.cs
+++ ProAgil.WebAPI/Startup.cs
@@ -23,6 +23,7 @@ namespace ProAgil.WebAPI
             services.AddDbContext<DataContext>(
               Db => Db.UseSqlite(Configuration.GetConnectionString("DefaultConnection")));
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            services.AddCors();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -39,6 +40,8 @@ namespace ProAgil.WebAPI
             }
 
            // app.UseHttpsRedirection();
+            app.UseCors(x => x.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());    
+            app.UseStaticFiles();
             app.UseMvc();
         }
     }
